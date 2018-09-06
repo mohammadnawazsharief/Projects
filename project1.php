2d0332ade4c803c92cbecdc78ad80bc9d4327ef6
mohammadnawazsharief <40892813+mohammadnawazsharief@users.noreply.github.com>
2018-09-07 00:30:45 +0530

    Add files via upload

---
+++ project1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+      <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+  </head>
+  <link rel="stylesheet" href="styles.css">
+
+
+  <body>
+    <div id="navbar">
+      <button id="button1" type="button" name="fork">Fork</button>
+      <button id="button" type="button" name="login">Login</button>
+      <button id="button" type="button" name="signup">SignUp</button>
+      <div id="Brand">
+        <h3><a href="#">Mohammad Nawaz</a></h3>
+        
+      </div>
+    </div>
+      <div id="main">
+        <h1 id="title">Bhagat Singh</h1>
+        <div>The man who scarifised his life for the freedom of india  </div>
+              <div id="img-div">
+                  <img id="image" src="http://eshadoot.com/wp-content/uploads/2017/09/Bhagat-Singh.jpg" alt="Bhagat singh image">
+                  <div id="img-caption">
+                    Bhagat Singh was an Indian nationalist considered to be one of the most influential revolutionaries of the Indian independence movement.
+                  </div>
+              </div>
+              <div id="tribute-info">
+                <h3 id="headline">Revolutionary Activities</h3>
+                <ul>
+                  <li><strong>1907</strong>- Born In Pubjab Province -
+                    Bhagat Singh, a Sandhu Jat,[6] was born in 1907[a] to Kishan Singh and Vidyavati at Chak No. 105 GB, Banga village, Jaranwala Tehsil in the Lyallpur district of the Punjab Province of British India(present day Pakistan).
+                  </li>
+                  <li><strong>1928</strong>- Lala Lajpat Rai death and killing of saunders -
+                  In 1928, the British government set up the Simon Commission to report on the political situation in India. Some Indian political parties boycotted the Commission because there were no Indians in its membership,[e] and there were protests across the country. When the Commission visited Lahore on 30 October 1928, Lala Lajpat Rai led a march in protest against it.
+                  Police attempts to disperse the large crowd resulted in violence.
+                  </li>
+                  <li><strong>1929</strong>- Assembly Incidence-
+                    For some time, Singh had been exploiting the power of drama as a means to inspire the revolt against the British, purchasing a magic lantern to show slides that enlivened his talks about revolutionaries such as Ram Prasad Bismil who had died as a result of the Kakori conspiracy. In 1929, he proposed a dramatic act to the HSRA intended to gain massive publicity for their aims.[22] Influenced by Auguste Vaillant, a French anarchist who had bombed the Chamber of Deputies in Paris,[33] Singh's plan was to explode a bomb inside the Central Legislative Assembly.
+                  </li>
+                  <li><strong>1930</strong>-Special Tribunal
+                    To speed up the slow trial, the Viceroy, Lord Irwin, declared an emergency on 1 May 1930 and introduced an ordinance to set up a special tribunal composed of three high court judges for the case. This decision cut short the normal process of justice as the only appeal after the tribunal was to the Privy Council located in England.
+                  </li>
+                  <li><strong>1931</strong>- Execution
+                    Singh, Rajguru and Sukhdev were sentenced to death in the Lahore conspiracy case and ordered to be hanged on 24 March 1931. The schedule was moved forward by 11 hours and the three were hanged on 23 March 1931 at 7:30 pm[64] in the Lahore jail. It is reported that no magistrate at the time was willing to supervise Singh's hanging as was required by law.
+                  </li>
+                </ul>
+                <blockquote cite="http://">
+                  <p>This is not the time to marry. My country is calling me. I have taken a vow to serve the country with my heart and soul. </p>
+                </blockquote>
+              </div>
+              <div id="tribute-link">
+                  <a href="https://en.wikipedia.org/wiki/Bhagat_Singh" target="_blank">
+                    <b>Want to Learn more about Bhagat Singh?</b> Click here</a>
+              </div>
+
+      </div>
+  </body>
+</html>
